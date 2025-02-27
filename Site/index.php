66964b9d09af38855e0ffd9718e03d594392ff6d
Maria Yureva <manuna.95.95@gmail.com>
2025-02-27 21:11:22 +0300

    Delete Site directory

--- Site/index.php
+++
@@ -1,8 +0,0 @@
-<form method="get">
-    <input type="text" name="commentBodyTitle" placeholder="Введите текст">
-    <input type="submit" value="Найти" name="submitButton">
-</form>
-
-<?php
-require 'filter.php'
-?>
